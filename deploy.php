ecf1a639b8e47de4d3f89c27049d75a37e7f6c99
RealZark <35647209@qq.com>
2019-12-16 12:08:26 +0800

    初始化样式

--- deploy.php
+++ deploy.php
@@ -13,7 +13,7 @@ set('repository', 'git@github.com:RealZark/weibo.git');
 set('git_tty', true);
 
 // Shared files/dirs between deploys
-add('shared_files', ['.env']);
+add('shared_files', []);
 add('shared_dirs', []);
 
 // Writable dirs by web server
@@ -24,6 +24,7 @@ add('writable_dirs', []);
 
 host('106.15.36.187')
     ->user('deployer')
+    ->stage('production')
     ->identityFile('~/.ssh/deployerkey')
     ->set('deploy_path', '/www/wwwroot/weibo');
 
@@ -33,10 +34,18 @@ task('build', function () {
     run('cd {{release_path}} && build');
 });
 
+task('reload:php-fpm', function () {
+    run('sudo /usr/sbin/service php7-fpm reload');
+});
+
+desc('Reload php-fpm');
+after('deploy', 'reload:php-fpm');
+
+desc('Restart horizon queue');
+after('deploy', 'artisan:horizon:terminate');
+
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
-
 before('deploy:symlink', 'artisan:migrate');
-
